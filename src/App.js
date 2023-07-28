171b615a8fec54dbde7db4b98ca7e4b0c8f16362
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-28 19:13:52 +0530

    React JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Navs from './componets/video';
+
+
+
+let logo= "https://thumbs.dreamstime.com/b/sunrise-over-beach-cancun-beautiful-mexico-40065727.jpg";
+
+
+let firstName="Esha";
+let lastName="Patel";
+let age=19;
+let city="surat";
+
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1 className="nav">Esha</h1>
+    <img src={logo} className="App-logo" alt="logo" />
+    <p className="navb">{firstName}</p>
+    <p className="navb">{lastName}</p>
+    <p className="navb">{age}</p>
+    <p className="navb">{city}</p>
+
+    
+    <Navs title="React js tutorials"  company="The Compass"  views="20k" year="2021" button="React Js"></Navs>
+    <Navs title="node js tutorials"  company="The Compass"  views="30k" year="2023" button="node Js"></Navs>
+    <Navs title="Full Stack tutorials"  company="The Compass"  views="40k" year="2022" button="Full Stack Js"></Navs>
+
+
+    
     </div>
   );
 }
+
 
 export default App;

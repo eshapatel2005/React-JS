2d3f4974fe100a9a56c96b234902779f871686cb
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-29 16:23:49 +0530

    simple project create

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
 import './App.css';
 
-import Navs from './componets/video';
-
-
-
-let logo= "https://thumbs.dreamstime.com/b/sunrise-over-beach-cancun-beautiful-mexico-40065727.jpg";
-
-
-let firstName="ESHA";
-let lastName="Patel";
-let age=19;
-let city="surat";
-
-
+let name = "react-app"
 function App() {
-
   return (
-    <div>
-    <h1 className="nav">Esha</h1>
-    <img src={logo} className="App-logo" alt="logo" />
-    <p className="navb">{firstName}</p>
-    <p className="navb">{lastName}</p>
-    <p className="navb">{age}</p>
-    <p className="navb">{city}</p>
-
-    
-    <Navs title="React js tutorials"  company="The Compass"  views="20k" year="2021" button="React Js"></Navs>
-    <Navs title="node js tutorials"  company="The Compass"  views="30k" year="2023" button="node JS"></Navs>
-    <Navs title="Full Stack tutorials"  company="The Compass"  views="40k" year="2022" button="Full Stack JS"></Navs>
+    <div className="App">
+      <div className="App-header">
+        Hello World
 
 
-    
+        {name}
+        <Demo></Demo>
+        </div>
     </div>
   );
 }
-
-
+function Demo() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        This is a demo
+        </div>
+    </div>
+  );
+}
 export default App;
